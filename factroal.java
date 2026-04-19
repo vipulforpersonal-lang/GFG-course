92289b70c5f3d734a1200030bf36ff16cf77ba2a
vipul mishra <vipulforpersonal@gmail.com>
2026-04-19 09:56:47 +0530

    Add files via upload

---
+++ factroal.java
@@ -0,0 +1,26 @@
+public class factroal {
+    public static void main(String[] args) {
+        /*sc = new Scanner(System.in);
+        System.out.println("enter the number ");
+        int n = sc.nextInt();
+
+        int factoril = 1;
+        for (int i = 1; i <= n; i++)
+            factoril = factoril * i;
+        System.out.println(factoril);*/
+
+
+
+
+        // FOR recursive solution 
+        public static  fac(int n ){
+            if (n==0){
+                return 0;
+
+            }else {
+
+            }
+        }
+
+    }
+}
